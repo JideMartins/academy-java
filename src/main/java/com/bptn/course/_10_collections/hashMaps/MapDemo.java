1f9b1bb7d99c456993f89b2b590351c557ac7ed0
JideMartins <jeedeyy@gmail.com>
2024-10-09 22:10:59 -0400

    hashMap

--- src/main/java/com/bptn/course/_10_collections/hashMaps/MapDemo.java
+++ src/main/java/com/bptn/course/_10_collections/hashMaps/MapDemo.java
@@ -6,7 +6,7 @@ public class MapDemo {
 
 	public static void main(String[] args) {
 		HashMap<Integer, String> numbers = new HashMap<>();
-		
+
 		numbers.put(1, "one");
 		numbers.put(2, "two");
 		numbers.put(3, "three");
@@ -17,25 +17,22 @@ public class MapDemo {
 		numbers.put(8, "eight");
 		numbers.put(9, "nine");
 		numbers.put(10, "ten");
-		
-		//printing map generated
+
+		// printing map generated
 		for (Integer i : numbers.keySet()) {
 			System.out.println(i + " : " + numbers.get(i));
 		}
-		
-		
-		//remove key 7
+
+		// remove key 7
 		numbers.remove(7);
-		
-		//add 11
+
+		// add 11
 		numbers.put(11, "eleven");
-		
-		//print updated list
-		for(Integer i: numbers.keySet()) {
+
+		// print updated list
+		for (Integer i : numbers.keySet()) {
 			System.out.println(i + " : " + numbers.get(i));
 		}
-		
-		
 
 	}
 
